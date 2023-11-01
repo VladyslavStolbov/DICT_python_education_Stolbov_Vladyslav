bb8b48f194e07cfe459e60317e1d1b8532bac3c2
VladyslavStolbov <stolbovvladislav20@gmail.com>
2023-11-01 20:58:35 +0200

    Hangman 2-nd stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,3 +1,11 @@
 """ Hangman project """
+word = "python"
+
 print("HANGMAN")
 print("The game will be available soon.")
+guess_word = input("Guess the word:")
+
+if guess_word == word:
+    print("You survived!")
+else:
+    print("You lost!")
