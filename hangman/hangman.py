f58198323937da4c1efedbd8a9b57343096a86e7
VladyslavStolbov <stolbovvladislav20@gmail.com>
2023-11-01 21:04:43 +0200

    Hangman 3-rd stage

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,11 +1,14 @@
 """ Hangman project """
-word = "python"
+import random
+
+words = ["python", "java", "javascript", "php"]
+random_word = random.choice(words)
 
 print("HANGMAN")
 print("The game will be available soon.")
 guess_word = input("Guess the word:")
 
-if guess_word == word:
+if guess_word == random_word:
     print("You survived!")
 else:
     print("You lost!")
